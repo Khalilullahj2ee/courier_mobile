194fb2b4d5de1fd1c9ebcdaccdbf2c96a02e7828
Khalilullahj2ee <atmkhalilullah91@gmail.com>
2022-02-19 13:13:12 +0600

    card

--- lib/view/admin_panel.dart
+++ lib/view/admin_panel.dart
@@ -15,6 +15,34 @@ class _AdminPanalState extends State<AdminPanal> {
         appBar: AppBar(
 
         title: Text("Welcome To Admin Pannal"),),
+      body: const Dashboard(),
     );
   }
 }
+
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  _DashboardState createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.album, size: 45),
+            title: Text('Sonu Nigam'),
+            subtitle: Text('Best of Sonu Nigam Song'),
+          ),
+        ],
+      ),
+    );
+}
+
+ }
